97f9d4a61fdc6831bb4836ab2be1d22574ce534f
Vijayabalaji B <jayb2495@gmail.com>
2024-01-28 16:32:51 +0530

    LL constructor

--- 01-Linked Lists/EXERCISE-LL-Constructor.py
+++ 01-Linked Lists/EXERCISE-LL-Constructor.py
@@ -1,22 +1,17 @@
-# class Node:
-    ## WRITE NODE CONSTRUCTOR HERE ##
-    #                               #
-    #                               #
-    #                               #
-    #                               #
-    #################################
-        
-# class LinkedList:
-    ## WRITE LL CONSTRUCTOR HERE ##
-    #                             #
-    #                             #
-    #                             #
-    #                             #
-    ###############################
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
 
 
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
 
- 
+
 my_linked_list = LinkedList(4)
 
 print('Head:', my_linked_list.head.value)
@@ -32,5 +27,3 @@ print('Length:', my_linked_list.length)
     Length: 1
     
 """
-
-                                                                                                                    
